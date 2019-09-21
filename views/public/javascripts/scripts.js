02d5d6c12df86226f9a6c146638d5c8525aadbed
Jewel Posniak <jewelposniak@gmail.com>
2019-09-21 19:09:25 -0400

    initial commit

---
+++ views/public/javascripts/scripts.js
@@ -0,0 +1,24 @@
+'use strict';
+
+class ShoppingList extends React.Component {
+  render() {
+    return (
+      <div className="shopping-list">
+        <h1>Shopping List for {this.props.name}</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+// ReactDOM.render(
+//   <ShoppingList />,
+//   document.getElementById('root')
+// );
+
+  const domContainer = document.querySelector('#hello');
+  ReactDOM.render(e(ShoppingList), domContainer);
